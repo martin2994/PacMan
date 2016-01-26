5a05aaddf19169f03e5f4a09b29c282b88e00dbb
Sangmystique <tchezmoi@gmail.com>
2016-01-26 15:39:01 +0100

    aa
    za

---
+++ PacmanTest/src/IOTreatment.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class IOTreatment {
+	
+	public static int[][] readMatrix(int [][]tab) throws IOException {
+			File f = new File("matrix.txt");
+			FileInputStream ips = new FileInputStream(f);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String currentLine;
+			int width = Integer.parseInt(br.readLine().toString());
+			int height = Integer.parseInt(br.readLine().toString());
+			//int tab[][] = new int[height][width];
+			int count=0;
+			while ((currentLine=br.readLine())!=null){
+				StringTokenizer currentChar = new StringTokenizer(currentLine);
+				for (int i=0 ; currentChar.hasMoreTokens() ; i++){
+					tab[i][count]=Integer.parseInt(currentChar.nextToken().toString());
+				}
+				count++;
+			}
+			/*for (int i=0 ; i<height ; i++){
+				for (int j=0 ; j<width ; j++){
+					System.out.print(tab[i][j]+" ");
+				}
+				System.out.println();
+			}*/
+			br.close();
+			return tab;
+	}
+
+	public static int getWidth() throws IOException {
+		File f = new File("matrix.txt");
+		FileInputStream ips = new FileInputStream(f);
+		InputStreamReader ipsr = new InputStreamReader(ips);
+		BufferedReader br = new BufferedReader(ipsr);
+		return Integer.parseInt(br.readLine().toString());
+	}
+
+	public static int getHeigth() throws IOException {
+		File f = new File("matrix.txt");
+		FileInputStream ips = new FileInputStream(f);
+		InputStreamReader ipsr = new InputStreamReader(ips);
+		BufferedReader br = new BufferedReader(ipsr);
+		int width=Integer.parseInt(br.readLine().toString());
+		return Integer.parseInt(br.readLine().toString()) ;
+	}
+
+
+}
---
+++ PacmanTest/src/Vue.java
@@ -0,0 +1,40 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class Vue extends JFrame {
+	
+	Panneau pan;
+	
+	public Vue(Controleur controle, int x, int y, int maxX, int maxY, Ghost blinky, Ghost pinky, Ghost inky, Ghost clyde, Modele.Direction go){
+		pan = new Panneau();
+		this.setTitle("Pac-Man");
+		this.setSize(maxX,maxY);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setContentPane(pan);
+		pan.setPosX(x);
+		pan.setPosY(y);
+		pan.setBlinky(blinky);
+		pan.setPinky(pinky);
+		pan.setInky(inky);
+		pan.setClyde(clyde);
+		pan.setGo_pacman(go);
+		addKeyListener(controle);		
+		addWindowListener(new WindowAdapter(){
+			public void windowOpened(WindowEvent e){
+				requestFocus();
+			}	
+		});
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	
+	public void refresh (int x, int y,Modele.Direction go){
+		pan.setPosX(x);
+		pan.setPosY(y);
+		pan.setGo_pacman(go);
+		pan.repaint();
+	}
+}
