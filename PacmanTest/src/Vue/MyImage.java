6b24fb2185aac2a50e8323600744b45772043608
martin2994 <martin2994@hotmail.fr>
2016-03-14 12:52:16 +0100

    Déplacement image + @return

--- PacmanTest/src/Vue/MyImage.java
+++ PacmanTest/src/Vue/MyImage.java
@@ -46,64 +46,64 @@ public class MyImage {
 		mypacman = new Image[5];
 		myghost = new Image[22];
 		try { // chargement des images
-			mylab[0] = ImageIO.read(new File("src/image/case0.png"));
-			mylab[1] = ImageIO.read(new File("src/image/case1.png"));
-			mylab[26] = ImageIO.read(new File("src/image/case-5.png"));
-			mylab[2] = ImageIO.read(new File("src/image/case2.png"));
-			mylab[3] = ImageIO.read(new File("src/image/case3.png"));
-			mylab[4] = ImageIO.read(new File("src/image/case4.png"));
-			mylab[5] = ImageIO.read(new File("src/image/case5.png"));
-			mylab[6] = ImageIO.read(new File("src/image/case6.png"));
-			mylab[7] = ImageIO.read(new File("src/image/case7.png"));
-			mylab[8] = ImageIO.read(new File("src/image/case8.png"));
-			mylab[9] = ImageIO.read(new File("src/image/case9.png"));
-			mylab[10] = ImageIO.read(new File("src/image/case10.png"));
-			mylab[11] = ImageIO.read(new File("src/image/case11.png"));
-			mylab[12] = ImageIO.read(new File("src/image/case12.png"));
-			mylab[13] = ImageIO.read(new File("src/image/case13.png"));
-			mylab[14] = ImageIO.read(new File("src/image/case14.png"));
-			mylab[15] = ImageIO.read(new File("src/image/case15.png"));
-			mylab[16] = ImageIO.read(new File("src/image/case16.png"));
-			mylab[17] = ImageIO.read(new File("src/image/case17.png"));
-			mylab[18] = ImageIO.read(new File("src/image/case18.png"));
-			mylab[19] = ImageIO.read(new File("src/image/case19.png"));
-			mylab[20] = ImageIO.read(new File("src/image/case20.png"));
-			mylab[21] = ImageIO.read(new File("src/image/case21.png"));
-			mylab[22] = ImageIO.read(new File("src/image/case22.png"));
-			mylab[23] = ImageIO.read(new File("src/image/case23.png"));
-			mylab[24] = ImageIO.read(new File("src/image/case24.png"));
-			mylab[25] = ImageIO.read(new File("src/image/case25.png"));
-			mypacman[1] = ImageIO.read(new File("src/image/Pacman_left.png"));
-			mypacman[2] = ImageIO.read(new File("src/image/Pacman_right.png"));
-			mypacman[3] = ImageIO.read(new File("src/image/Pacman_up.png"));
-			mypacman[4] = ImageIO.read(new File("src/image/Pacman_down.png"));
-			mypacman[0] = ImageIO.read(new File("src/image/Pacman_close.png"));
-			myghost[0] = ImageIO.read(new File("src/image/Blinky_left.png"));
-			myghost[1] = ImageIO.read(new File("src/image/Blinky_up.png"));
-			myghost[2] = ImageIO.read(new File("src/image/Blinky_down.png"));
-			myghost[3] = ImageIO.read(new File("src/image/Blinky_right.png"));
-			myghost[4] = ImageIO.read(new File("src/image/Clyde_left.png"));
-			myghost[5] = ImageIO.read(new File("src/image/Clyde_up.png"));
-			myghost[6] = ImageIO.read(new File("src/image/Clyde_down.png"));
-			myghost[7] = ImageIO.read(new File("src/image/Clyde_right.png"));
-			myghost[8] = ImageIO.read(new File("src/image/Inky_left.png"));
-			myghost[9] = ImageIO.read(new File("src/image/Inky_up.png"));
-			myghost[10] = ImageIO.read(new File("src/image/Inky_down.png"));
-			myghost[11] = ImageIO.read(new File("src/image/Inky_right.png"));
-			myghost[12] = ImageIO.read(new File("src/image/Pinky_left.png"));
-			myghost[13] = ImageIO.read(new File("src/image/Pinky_up.png"));
-			myghost[14] = ImageIO.read(new File("src/image/Pinky_down.png"));
-			myghost[15] = ImageIO.read(new File("src/image/Pinky_right.png"));
-			myghost[20] = ImageIO.read(new File("src/image/Blue_Ghost.png"));
-			myghost[21] = ImageIO.read(new File("src/image/White_Ghost.png"));
-			myghost[19] = ImageIO.read(new File("src/image/eyes_right.png"));
-			myghost[17] = ImageIO.read(new File("src/image/eyes_up.png"));
-			myghost[18] = ImageIO.read(new File("src/image/eyes_down.png"));
-			myghost[16] = ImageIO.read(new File("src/image/eyes_left.png"));
-			mybonus[0] = ImageIO.read(new File("src/image/fraise.png"));
-			mybonus[1] = ImageIO.read(new File("src/image/orange.png"));
-			mybonus[2] = ImageIO.read(new File("src/image/cerise.png"));
-			mybonus[3] = ImageIO.read(new File("src/image/pomme.png"));
+			mylab[0] = ImageIO.read(new File("src/Vue/image/case0.png"));
+			mylab[1] = ImageIO.read(new File("src/Vue/image/case1.png"));
+			mylab[26] = ImageIO.read(new File("src/Vue/image/case-5.png"));
+			mylab[2] = ImageIO.read(new File("src/Vue/image/case2.png"));
+			mylab[3] = ImageIO.read(new File("src/Vue/image/case3.png"));
+			mylab[4] = ImageIO.read(new File("src/Vue/image/case4.png"));
+			mylab[5] = ImageIO.read(new File("src/Vue/image/case5.png"));
+			mylab[6] = ImageIO.read(new File("src/Vue/image/case6.png"));
+			mylab[7] = ImageIO.read(new File("src/Vue/image/case7.png"));
+			mylab[8] = ImageIO.read(new File("src/Vue/image/case8.png"));
+			mylab[9] = ImageIO.read(new File("src/Vue/image/case9.png"));
+			mylab[10] = ImageIO.read(new File("src/Vue/image/case10.png"));
+			mylab[11] = ImageIO.read(new File("src/Vue/image/case11.png"));
+			mylab[12] = ImageIO.read(new File("src/Vue/image/case12.png"));
+			mylab[13] = ImageIO.read(new File("src/Vue/image/case13.png"));
+			mylab[14] = ImageIO.read(new File("src/Vue/image/case14.png"));
+			mylab[15] = ImageIO.read(new File("src/Vue/image/case15.png"));
+			mylab[16] = ImageIO.read(new File("src/Vue/image/case16.png"));
+			mylab[17] = ImageIO.read(new File("src/Vue/image/case17.png"));
+			mylab[18] = ImageIO.read(new File("src/Vue/image/case18.png"));
+			mylab[19] = ImageIO.read(new File("src/Vue/image/case19.png"));
+			mylab[20] = ImageIO.read(new File("src/Vue/image/case20.png"));
+			mylab[21] = ImageIO.read(new File("src/Vue/image/case21.png"));
+			mylab[22] = ImageIO.read(new File("src/Vue/image/case22.png"));
+			mylab[23] = ImageIO.read(new File("src/Vue/image/case23.png"));
+			mylab[24] = ImageIO.read(new File("src/Vue/image/case24.png"));
+			mylab[25] = ImageIO.read(new File("src/Vue/image/case25.png"));
+			mypacman[1] = ImageIO.read(new File("src/Vue/image/Pacman_left.png"));
+			mypacman[2] = ImageIO.read(new File("src/Vue/image/Pacman_right.png"));
+			mypacman[3] = ImageIO.read(new File("src/Vue/image/Pacman_up.png"));
+			mypacman[4] = ImageIO.read(new File("src/Vue/image/Pacman_down.png"));
+			mypacman[0] = ImageIO.read(new File("src/Vue/image/Pacman_close.png"));
+			myghost[0] = ImageIO.read(new File("src/Vue/image/Blinky_left.png"));
+			myghost[1] = ImageIO.read(new File("src/Vue/image/Blinky_up.png"));
+			myghost[2] = ImageIO.read(new File("src/Vue/image/Blinky_down.png"));
+			myghost[3] = ImageIO.read(new File("src/Vue/image/Blinky_right.png"));
+			myghost[4] = ImageIO.read(new File("src/Vue/image/Clyde_left.png"));
+			myghost[5] = ImageIO.read(new File("src/Vue/image/Clyde_up.png"));
+			myghost[6] = ImageIO.read(new File("src/Vue/image/Clyde_down.png"));
+			myghost[7] = ImageIO.read(new File("src/Vue/image/Clyde_right.png"));
+			myghost[8] = ImageIO.read(new File("src/Vue/image/Inky_left.png"));
+			myghost[9] = ImageIO.read(new File("src/Vue/image/Inky_up.png"));
+			myghost[10] = ImageIO.read(new File("src/Vue/image/Inky_down.png"));
+			myghost[11] = ImageIO.read(new File("src/Vue/image/Inky_right.png"));
+			myghost[12] = ImageIO.read(new File("src/Vue/image/Pinky_left.png"));
+			myghost[13] = ImageIO.read(new File("src/Vue/image/Pinky_up.png"));
+			myghost[14] = ImageIO.read(new File("src/Vue/image/Pinky_down.png"));
+			myghost[15] = ImageIO.read(new File("src/Vue/image/Pinky_right.png"));
+			myghost[20] = ImageIO.read(new File("src/Vue/image/Blue_Ghost.png"));
+			myghost[21] = ImageIO.read(new File("src/Vue/image/White_Ghost.png"));
+			myghost[19] = ImageIO.read(new File("src/Vue/image/eyes_right.png"));
+			myghost[17] = ImageIO.read(new File("src/Vue/image/eyes_up.png"));
+			myghost[18] = ImageIO.read(new File("src/Vue/image/eyes_down.png"));
+			myghost[16] = ImageIO.read(new File("src/Vue/image/eyes_left.png"));
+			mybonus[0] = ImageIO.read(new File("src/Vue/image/fraise.png"));
+			mybonus[1] = ImageIO.read(new File("src/Vue/image/orange.png"));
+			mybonus[2] = ImageIO.read(new File("src/Vue/image/cerise.png"));
+			mybonus[3] = ImageIO.read(new File("src/Vue/image/pomme.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -114,6 +114,7 @@ public class MyImage {
 	 * 
 	 * @param i
 	 *            ième case du tableau
+	 * @return l'image correspondante 
 	 */
 	public Image getCase(int i) {
 		return mylab[i];
@@ -126,6 +127,7 @@ public class MyImage {
 	 *            nom du fantôme
 	 * @param direction
 	 *            direction du fantôme
+	 * @return l'image correspondante            
 	 */
 	public Image getGhost(String name, String direction) {
 		int i;
@@ -204,6 +206,7 @@ public class MyImage {
 	 * 
 	 * @param name
 	 *            nom du bonus
+	 * @return l'image correspondante            
 	 */
 	public Image getBonus(String name) {
 		switch (name) {
@@ -225,6 +228,7 @@ public class MyImage {
 	 * 
 	 * @param name
 	 *            nom de la direction du pacman
+	 * @return l'image correspondante           
 	 */
 	public Image getPacman(String name) {
 		switch (name) {
