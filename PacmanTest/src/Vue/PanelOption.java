3d21b419e24a38b7ec4ba117fff6c7fb27be8424
Sangmystique <tchezmoi@gmail.com>
2016-03-16 22:30:40 +0100

    update panel option trop cool

--- PacmanTest/src/Vue/PanelOption.java
+++ PacmanTest/src/Vue/PanelOption.java
@@ -3,7 +3,10 @@ package Vue;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
@@ -17,6 +20,20 @@ public class PanelOption extends JPanel {
 	/**
 	 * Affiche la page
 	 */
+	
+
+	private Image pacman_up;
+	
+	public PanelOption(){
+
+		try {
+			pacman_up=ImageIO.read(Vue.class.getResource("image/Pacman_up.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public void paintComponent(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
@@ -33,5 +50,10 @@ public class PanelOption extends JPanel {
 		g.drawLine(0,150,this.getWidth(),150);
 		g.drawLine(0,200,this.getWidth(),200);
 		g.drawLine(this.getWidth()/2,0,this.getWidth()/2,225);
+		g.drawLine(this.getWidth()/2-1,0,this.getWidth()/2-1,225);
+		g.fillOval(this.getWidth()/2-1,235,2,2);
+		g.fillOval(this.getWidth()/2-2,250,4,4);
+		g.fillOval(this.getWidth()/2-3,270,6,6);
+		g.drawImage(pacman_up, this.getWidth()/2-13,280, this);
 	}
 }
